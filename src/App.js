2d6ca9177c516055410690588801bf5bd773dc22
Jashawa <jashawaparnell@gmail.com>
2024-09-21 07:39:30 -0700

    Imported all components from Components folder to App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
+import Header from './Components.js/Header';
+import Nav from './Components.js/Nav';
+import Main from './Components.js/Main';
+import Footer from './Components.js/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Home Page</h1>
+      <Header />
+      <Nav />
+      <Main />
+      < Footer />
     </div>
   );
 }
