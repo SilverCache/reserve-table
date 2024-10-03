5c160c718b9fe373b4f1547c804c66bbc5acb102
Jashawa <jashawaparnell@gmail.com>
2024-10-03 06:40:48 -0700

    Nav bar complete

--- src/Components.js/Nav.js
+++ src/Components.js/Nav.js
@@ -1,16 +1,48 @@
-function Nav() {
+import React, { useState } from "react";
+import logo from "./Images.js/Logo.svg";
+
+const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav className="main-nav">
-      <ul>
-        <li><a href="/Home">Home</a></li>
-        <li><a href="/About">About</a></li>
-        <li><a href="/Menu">Menu</a></li>
-        <li><a href="/Reservations">Reservations</a></li>
-        <li><a href="/Order">Order</a></li>
-        <li><a href="/log In">log In</a></li>
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+      <a href="/" className="logo">
+        <img src={logo} alt="" />
+      </a>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Services</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservations</a>
+        </li>
+        <li>
+          <a href="/booking">Order Online</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Nav;
