57601f189c616266b9edf1f58e618f75c0333928
Jashawa <jashawaparnell@gmail.com>
2024-10-03 07:50:47 -0700

    added img to header. header complete

--- src/Components.js/Header.js
+++ src/Components.js/Header.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import logo from './Images.js/Logo.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import bannerImg from "./Images.js/restauranfood.jpg";
 
-console. log(logo);
-
-function Header() {
+const Header = () => {
   return (
-    <img src={logo} alt='Logo' />
-  )
-}
+    <header className="header">
+      <section>
+        <div className="banner">
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            We are a family owned Mediterraneran restaurant, focused on
+            traditional recipes servred with a modern twist.
+          </p>
+          <Link to="/booking">
+            <button aria-label="On Click">Reserve Table</button>
+          </Link>
+        </div>
+        <div className="banner-img">
+          <img src={bannerImg} alt="" />
+        </div>
+      </section>
+    </header>
+  );
+};
 
 export default Header;
